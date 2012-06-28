1c2e9c541b209b48f822c2efbdd81e37ee411b44
rbulha <rbulha@esd-talk.com>
2012-06-28 02:05:24 -0300

    -Used to hack the bluetooth module

--- host/src/serialmon.py
+++ host/src/serialmon.py
@@ -1,32 +1,76 @@
-import wx
+#import wx
 import os
 import sys
 import __builtin__
 import serial
 import time
+import threading
+import thread
 
 __builtin__.__dict__['HOMEPATH'] = os.path.abspath(os.path.dirname(sys.argv[0]))
 
+class BasicThread:
+    def __init__(self):
+        self.keepGoing = self.running = False
+        self.event = threading.Event()
+        self.event.clear()
+
+    def start(self):
+        self.keepGoing = self.running = True
+        thread.start_new_thread(self.run, ())
+
+    def stop(self):
+        self.keepGoing = self.running = False
+        
+    def wait(self, timeout):  
+        self.event.wait(timeout) 
+
+    def IsRunning(self):
+        return self.running
+
+    def run(self):
+        print 'rodando virtual'
+
+class SerialReception(BasicThread):
+  def __init__(self, indentificador, parent, serial_port):
+    BasicThread.__init__(self)
+    self.id = indentificador
+    self.parent = parent
+    self.serial_port = serial_port
+    self.Waiting = True
+    print "init thread: ", self.id
+  def run(self):
+    print 'running dialog, ',self.id 
+    count = 3
+    self.Waiting = True 
+    while self.IsRunning() and self.Waiting:
+        self.com_output = self.serial_port.read(256)
+        if len(self.com_output) > 0:
+            print '[serial monitor] *******>>>>>> output: ',self.com_output
+
 def main():
-            port_number = '/dev/tty'
-            s = serial.Serial(port_number,timeout=2)
-            print '[OnButton_wxButtonConnect] port name: %s'%s.portstr
-            #self.wxTextPortName.SetValue(str(s.portstr))
-            #time.sleep(5)
-            #command_list = str(self.wxTextConfigurationCommand.GetValue())
-            #print '[OnButton_wxButtonConnect] configuratin token: ',command_list
-            #s.write(command_list)
-            #time.sleep(5)
-            #com_output = s.read(100)
-            #print '[OnButton_wxButtonConnect] port output: ',com_output
+    if sys.platform == 'win32':
+        port_number = 2
+    else:
+        port_number = '/dev/tty'
+    retry_timeout = 3    
+    s = serial.Serial(port_number,timeout=2)
+    print '[serial monitor] port name: %s'%s.portstr
+    reception_thread = SerialReception('reception',None,s)
+    reception_thread.start()
+    counter = 50
+    commands = ['$$$\r\n','AT\r\n','AT+NAME?\r\n','AT','AT+NAME','+++\r\n','AT\r\n','AT+NAME?\r\n']
+    while counter > 0 :
+        for i in range(retry_timeout):
+            print 'write in %d sec'%(retry_timeout - i)
+            time.sleep(0.5)
+        counter = counter - 1
+        for cmd in commands:
+            print '[serial monitor] write(%d): %s'%(counter,cmd)
+            s.write(cmd)    
             time.sleep(1)
-            #serial_number = str(123)
-            equip_tag = 'AT\r\n' 
-            s.write(equip_tag)   
-            time.sleep(5)
-            com_output = s.read(10)
-            print '[OnButton_wxButtonConnect] port output: ',com_output
-            s.close()
+    reception_thread.stop()    
+    s.close()
                 
 if __name__ == '__main__':
     main()                                
