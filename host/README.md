bf36743fe9f6b3e7016fee1d1452ded399bbe9a6
rbulha <rbulha@esd-talk.com>
2012-07-02 20:15:15 -0300

    Python testing module.

--- host/README.md
+++ host/README.md
@@ -3,6 +3,9 @@ bluebread
 
 Host tools for Wireless prototype breadboard. 
 
+src/serialmon.py - (test module) Python software to open a serial port and send 
+some data. Used to evaluate the bluetooth module.
+
 Author
 ======
 Rogério Bulha Siqueira <www.esd-talk.com>
--- host/src/serialmon.py
+++ host/src/serialmon.py
@@ -44,13 +44,13 @@ class SerialReception(BasicThread):
     count = 3
     self.Waiting = True 
     while self.IsRunning() and self.Waiting:
-        self.com_output = self.serial_port.read(256)
+        self.com_output = self.serial_port.readline(256)
         if len(self.com_output) > 0:
-            print '[serial monitor] *******>>>>>> output: ',self.com_output
+            print '[serial monitor] ***>>> output: [%s]'%self.com_output.strip(' \r\n')
 
 def main():
     if sys.platform == 'win32':
-        port_number = 2
+        port_number = 4
     else:
         port_number = '/dev/tty'
     retry_timeout = 3    
@@ -58,12 +58,12 @@ def main():
     print '[serial monitor] port name: %s'%s.portstr
     reception_thread = SerialReception('reception',None,s)
     reception_thread.start()
-    counter = 50
+    counter = 10
     commands = ['$$$\r\n','AT\r\n','AT+NAME?\r\n','AT','AT+NAME','+++\r\n','AT\r\n','AT+NAME?\r\n']
     while counter > 0 :
         for i in range(retry_timeout):
             print 'write in %d sec'%(retry_timeout - i)
-            time.sleep(0.5)
+            time.sleep(1)
         counter = counter - 1
         for cmd in commands:
             print '[serial monitor] write(%d): %s'%(counter,cmd)
@@ -71,7 +71,33 @@ def main():
             time.sleep(1)
     reception_thread.stop()    
     s.close()
+    
+def bluetooth_pooling():
+    if sys.platform == 'win32':
+        port_number = 4
+    else:
+        port_number = '/dev/tty'
+    retry_timeout = 3    
+    s = serial.Serial(port_number,timeout=2)
+    print '[serial monitor] port name: %s'%s.portstr
+    reception_thread = SerialReception('reception',None,s)
+    reception_thread.start()
+    counter = 10
+    commands = ['a','t','v']
+    while counter > 0 :
+        for i in range(retry_timeout):
+            print '[serial monitor] write in %d sec'%(retry_timeout - i)
+            time.sleep(2)
+        counter = counter - 1
+        for cmd in commands:
+            print '[serial monitor] write(%d): %s'%(counter,cmd)
+            s.write(cmd)    
+            time.sleep(1)
+    reception_thread.stop()    
+    s.close()
+    
                 
 if __name__ == '__main__':
-    main()                                
+    #main()
+    bluetooth_pooling()                               
 
